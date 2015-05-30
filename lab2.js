0cc9d5e3a2f332caa5f54d283fc117d3afc12a3f
stephanielingwood <stephanielingwood@gmail.com>
2015-05-30 11:55:38 -0700

    revised blob and sorting Qs; changed jshint/jscs options

--- lab2.js
+++ lab2.js
@@ -1,17 +1,18 @@
-/*********************************************************
-LAB 3:
+'use strict';
 
-Welcome to Lab 2 =)
+// LAB 2: SORTING AND CAMPY SCI-FI
 
-Be sure to read all the comments!
+// Welcome to Lab 2 =)
 
-All of the instructions are inline with the assignment below.
-Look for the word TODO in comments.  Each TODO will have a
-description of what is required.
+// Be sure to read all the comments!
 
-To run this file (in the terminal) use: node lab2.js
+// All of the instructions are inline with the assignment below.
+// Look for the word TODO in comments.  Each TODO will have a
+// description of what is required.
 
-*********************************************************/
+// To run this file (in the terminal) use: node lab2.js
+
+//*********************************************************
 // SETUP
 //*********************************************************
 
@@ -19,109 +20,113 @@ To run this file (in the terminal) use: node lab2.js
 // test our code
 function assert(expression, failureMessage) {
   if (!expression) {
-    console.log("assertion failure: ", failureMessage);
+    console.log('assertion failure: ', failureMessage);
   }
 }
 
 //*********************************************************
-// PROBLEM 1: What's in a Name? 20 points.
-//
-// Each of the following three functions (x, y, and z) has
-// an empty body. Look at the asserts for each function to
-// figure out what the function is supposed to do. Rename
-// each function to reflect its purpose, implement it, then
-// add two more passing assertions for each one.
+// PROBLEM 1: The Blob. 20 points
 //*********************************************************
 
-function x() {
-  //TODO: implement this function so that all the asserts
-  // for it pass.
-  //TODO: rename this function to match what it does
+/* Dowington, PA had 1000 citizens on the night the blob escaped
+ its meteorite. At first, the blob could only find and consume
+ Pennsylvanians at a rate of 1/hour. However, each time it digested
+ someone, it became faster and stronger: adding to its consumption
+ rate by 1 person/hour.
+
+    for the...      | starting rate of | persons consumed |
+                    |  consumption     |    that hour     |
+--------------------|------------------|------------------|
+    first hour      |    1/hour        |        1         |
+    second hour     |    2/hour        |        2         |
+    third hour      |    3/hour        |        3         |
+    fourth hour     |    4/hour        |        4         |
+
+ TODO: First, make a constructor function, called Blob, that makes blobs.
+
+ TODO: Next, create an instance of Blob named blob.
+
+ TODO: Then, use a loop to calculate how long it took the blob to finish
+ with Dowington.
+*/
+
+var hoursSpentInDowington; // TODO: assign me the value of the
+                           // above calculation (how long it took
+                           // the blob to eat Dowington)
+
+// Now, write a method that takes a population for an arbitrary
+// town, and the starting consumption rate, and returns the number
+// of hours the blob needs to ooze its way through that town.
+
+function hoursToOoze(population, peoplePerHour) {
+  // TODO: implement me based on the instructions above.
+  // Be sure to then assign me to the Blob's prototype.
 }
 
-// TODO: don't forget to rename the function down here, too
-assert(6.28 < x(1) && x(1) < 6.2832, "x(1)");
-assert(12.56 < x(2) && x(2) < 12.5664, "x(2)");
-assert(18.84 < x(3) && x(3) < 18.8496, "x(3)");
+assert(blob.hoursToOoze(0, 1) === 0, 'no people means no time needed.');
+assert(blob.hoursToOoze(1000, 1) === hoursSpentInDowington,
+  'hoursSpentInDowington should match hoursToOoze\'s result for 1000');
 
-function y() {
-  //TODO: implement this function so that all the asserts
-  // for it pass.
-  //TODO: rename this function to match what it does
-}
-
-// TODO: don't forget to rename the function down here, too
-assert(y(1, 2, 3) === 1, "y(1, 2, 3)");
-assert(y(4, 5, 3) === 3, "y(4, 5, 3)");
-assert(y(-5, -10, 1) === -10, "y(-5, -10, 1)");
-
-function z() {
-  //TODO: implement this function so that all the asserts
-  // for it pass.
-  //TODO: rename this function to match what it does
-}
-
-// TODO: don't forget to rename the function down here, too
-assert(z(1) === 1, "z(1)");
-assert(z(3) === 27, "z(3)");
-assert(z(5) === 125, "z(5)");
+// TODO: write three more assertions like the two above, testing out
+// the hoursToOoze method.
 
 //*********************************************************
-// PROBLEM 2: May cause side effects. 20 points.
-//
-// Rewrite the following two functions (sameItems and
-// filter) to remove all side effects.
+// PROBLEM 2: Universal Translator. 20 points
 //*********************************************************
 
-// sameItems takes two arrays. It returns true if the arrays
-// contain the same elements. Ordering doesn't matter.
-// Otherwise, sameItems returns false.
-function sameItems(array1, array2) {
-  array1.sort();
-  array2.sort();
-  while (array1.pop() === array2.pop()) {
-    if (!array1.length) {
-      return true;
-    }
+var hello = {
+  klingon: 'nuqneH',  // home planet is Qo'noS
+  romulan: 'Jolan\'tru', // home planet is Romulus
+  'federation standard': 'hello' // home planet is Earth
+};
+
+// TODO: define a constructor that creates objects to represent
+// sentient beings. They have a home planet, a language that they
+// speak, and method (that you'll place on the prototype) called
+// sayHello.
+
+function SentientBeing () {
+  // TODO: specify a home planet and a language
+  // you'll need to add parameters to this constructor
+}
+
+// sb is a SentientBeing object
+function sayHello (sb) {
+    // TODO: say hello prints out (console.log's) hello in the
+    // language of the speaker, but returns it in the language
+    // of the listener (the sb parameter above).
+    // use the 'hello' object at the beginning of this exercise
+    // to do the translating
+
+    //TODO: put this on the SentientBeing prototype
   }
-  return false;
-}
 
-assert(sameItems([], []), "sameItems([],[])");
-assert(sameItems([ 1, 2, 3 ], [ 2, 3, 1 ]),
-  "sameItems([1,2,3],[2,3,1])");
-assert(!sameItems([], [ 1 ]), "sameItems([],[1])");
+// TODO: create three subclasses of SentientBeing, one for each
+// species above (Klingon, Human, Romulan).
 
-// filter takes an array of strings and a search string. It
-// returns an array that has only the strings from array
-// that include the search string
-function filter(array, searchString) {
-  var startLength = array.length,
-    currentString;
-  for (var i = 0; i < startLength; i++) {
-    currentString = array.pop();
-    if (currentString.indexOf(searchString) !== -1) {
-      array.unshift(currentString);
-    }
-  }
-  return array;
-}
+assert((new Human()).sayHello(new Klingon()) === 'nuqneH',
+  'the klingon should hear nuqneH');
 
-var catWords = [ "catnip", "kitty yawns", "kitten pile" ];
-var kittenWords = [ "kitty yawns", "kitten pile" ];
-assert(sameItems(kittenWords, filter(catWords, "kitt")), "filter");
+// TODO: write five more assertions, to complete all the possible
+// greetings between the three types of sentient beings you created above.
 
 //*********************************************************
 // PROBLEM 3: Sorting. 20 points.
 //
 // Implement the following functions. Write at least 2
-// assertions for each one
+// assertions for each one (the assertions are how you
+// will test your code)
 //*********************************************************
+
 function lastLetterSort(stringArray) {
-  function byLastLetter(array) {
+  function byLastLetter(a, b) {
     //TODO: implement me. sort the strings in alphabetical
     // order using their last letter
-    // read this: http://www.w3schools.com/jsref/jsref_sort.asp
+    // Read this about how the sort function works:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+    // this byLastLetter function is a "compare function"
+    // And check out the "comparing strings" section  here:
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
   }
   stringArray.sort(byLastLetter);
 }
